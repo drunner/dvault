d4fe5eb3758028824e97d5d4d4311ff74bcc3a00
Sam Barham <s.barham@adinstruments.com>
2016-03-30 21:48:14 +0000

    Migrate to dRunner2

--- usrlocalbin/vault-load-wrapper.sh
+++ usrlocalbin/vault-load-wrapper.sh
@@ -2,8 +2,8 @@
 
 mkdir -p /tmp/output
 vault-load.py "$1"
-sudo cp -R /tmp/output/* /output/.
+sudo cp -R /tmp/output/* /load/.
 if [ ! "${2:-}" == "0" ] && [ ! "${3:-}" == "0" ]; then
-   sudo chown -R "${2}:${3}" /output/*
-   sudo chmod -R 600 /output/*
+   sudo chown -R "${2}:${3}" /load/*
+   sudo chmod -R 600 /load/*
 fi
